f0bc099a34c70140c769a20a61962fd588de52dd
DuyCa <ca@wolffungame.com>
2024-02-01 18:26:07 +0700

    Update AssetRefGameObjectPrefab.cs

--- Packages/ZBase.Foundation.Pooling/ZBase.Foundation.Pooling.Addressables/Foundation/Prefabs/AssetRefGameObjectPrefab.cs
+++ Packages/ZBase.Foundation.Pooling/ZBase.Foundation.Pooling.Addressables/Foundation/Prefabs/AssetRefGameObjectPrefab.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
@@ -8,7 +9,8 @@ using UnityEngine.ResourceManagement.AsyncOperations;
 namespace ZBase.Foundation.Pooling.AddressableAssets
 {
     [Serializable]
-    public class AssetRefGameObjectPrefab : AssetRefPrefab<GameObject, AssetReferenceGameObject>
+    public class AssetRefGameObjectPrefab : AssetRefPrefab<GameObject, AssetReferenceGameObject>,
+        IEquatable<AssetRefGameObjectPrefab>, IEqualityComparer<AssetRefGameObjectPrefab>
     {
         protected override async UniTask<GameObject> Instantiate(
             AssetReferenceGameObject source, Transform parent, CancellationToken cancelToken = default)
@@ -23,5 +25,12 @@ namespace ZBase.Foundation.Pooling.AddressableAssets
             if (instance && Source != null)
                 Source.ReleaseInstance(instance);
         }
+
+        public bool Equals(AssetRefGameObjectPrefab other) => other != null && this.Source.Equals(other.Source);
+
+        public bool Equals(AssetRefGameObjectPrefab x, AssetRefGameObjectPrefab y)
+            => x != null && y != null && x.Source.Equals(y.Source);
+
+        public int GetHashCode(AssetRefGameObjectPrefab obj) => obj.Source.GetHashCode();
     }
 }

cfe4fda7f93c0979b10c455f6133c91248cc0883
DuyCa <ca@wolffungame.com>
2024-02-01 15:17:47 +0700

    Lazy pool

---
+++ Packages/ZBase.Foundation.Pooling/GameObjectLazyPool/GlobalPools/Extensions/LazyAssetRefGameObjectPool.cs
@@ -0,0 +1,23 @@
+﻿using Cysharp.Threading.Tasks;
+using ZBase.Foundation.Pooling.AddressableAssets;
+
+namespace ZBase.Foundation.Pooling.GameObject.LazyPool.Extensions
+{
+    
+    public static class LazyAssetRefGameObjectPool
+    {
+        private static GlobalAssetRefGameObjectPool GlobalGameObjectPool => SharedPool.Of<GlobalAssetRefGameObjectPool>();
+
+        public static async UniTask<UnityEngine.GameObject> Rent(AssetRefGameObjectPrefab gameObjectReference)
+            => await GlobalGameObjectPool.Rent(gameObjectReference);
+        
+        public static void Return(UnityEngine.GameObject gameObject)
+            => GlobalGameObjectPool.Return(gameObject);
+        
+        public static void Return(AssetRefGameObjectPrefab gameObjectReference, UnityEngine.GameObject gameObject)
+            => GlobalGameObjectPool.Return(gameObjectReference, gameObject);
+        
+        public static void ReleaseInstances(int keep, System.Action<UnityEngine.GameObject> onReleased = null)
+            => GlobalGameObjectPool.ReleaseInstances(keep, onReleased);
+    }
+}
---
+++ Packages/ZBase.Foundation.Pooling/GameObjectLazyPool/GlobalPools/Extensions/LazyGameObjectPool.cs
@@ -0,0 +1,23 @@
+﻿using Cysharp.Threading.Tasks;
+using ZBase.Foundation.Pooling.UnityPools;
+
+namespace ZBase.Foundation.Pooling.GameObject.LazyPool.Extensions
+{
+    
+    public static class LazyGameObjectPool
+    {
+        private static GlobalGameObjectPool GlobalGameObjectPool => SharedPool.Of<GlobalGameObjectPool>();
+
+        public static async UniTask<UnityEngine.GameObject> Rent(GameObjectPrefab gameObjectReference)
+            => await GlobalGameObjectPool.Rent(gameObjectReference);
+        
+        public static void Return(UnityEngine.GameObject gameObject)
+            => GlobalGameObjectPool.Return(gameObject);
+        
+        public static void Return(GameObjectPrefab gameObjectReference, UnityEngine.GameObject gameObject)
+            => GlobalGameObjectPool.Return(gameObjectReference, gameObject);
+        
+        public static void ReleaseInstances(int keep, System.Action<UnityEngine.GameObject> onReleased = null)
+            => GlobalGameObjectPool.ReleaseInstances(keep, onReleased);
+    }
+}
